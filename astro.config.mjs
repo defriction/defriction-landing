2cc670b80f857f795354ab970bc630b4a2c1d21c
Julian Franco <juliandanielfranco@gmail.com>
2026-06-22 19:58:20 -0500

    fix: remove base path, configure for defriction.org final domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://defriction.org',
-  base: '/defriction/', // TODO: Quitar esto cuando el dominio defriction.org apunte al VPS
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
